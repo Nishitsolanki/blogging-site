a63bcfcf777bae7de27dc97b4573cf8653c88492
Nisha282/training <sahunishaji1220@gmail.com>
2022-08-30 14:48:02 +0530

    ok

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,10 +2,7 @@ const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 
 const createUser = async function (abcd, xyz) {
-  //You can name the req, res objects anything.
-  //but the first parameter is always the request 
-  //the second parameter is always the response
-
+  
   let data = abcd.body;
   let savedData = await userModel.create(data);
   console.log(abcd.newAtribute);
