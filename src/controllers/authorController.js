b1b9a45815fa025608ee34a595a38edbb807cb33
Divyanshu kumar <divyanshupatle123@gmail.com>
2022-09-05 22:48:08 +0530

    validation by divyanshu

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -1,12 +1,43 @@
 const authorModel = require("../models/authorModel");
 
+const isValid = function (value) {
+  if (typeof value === "undefined" || value === Number || value === null) return false
+  if (typeof value === "string" && value.trim().length === 0) return false
+  return true
+}
+
+
 const authors = async function (req, res) {
   try {
     let data = req.body;
+    if(Object.keys(data).length==0){
+     return res.status(400).send({status:false, msg: "data is required" });
+    }
+    if(!isValid(data.fname)){
+      return res.status(400).send({status:false, msg: "fname is required" }); 
+    }
+
+    if(!isValid(data.lname)){
+      return res.status(400).send({status:false, msg: "lname is required" }); 
+    }
+
+    if(!isValid(data.title)){
+      return res.status(400).send({status:false, msg: "title is required" }); 
+    }
+
+    if(!isValid(data.email)){
+      return res.status(400).send({status:false, msg: "email is required" }); 
+    }
+
+    if(!isValid(data.password)){
+      return res.status(400).send({status:false, msg: "password is required" }); 
+    }
+    
+
     let savedData = await authorModel.create(data);
     res.status(201).send({ msg: savedData });
   } catch (err) {
-    res.status(500).send({ ErrorName: err.name, ErrorMessage: err.message });
+    return res.status(500).send({ ErrorName: err.name, ErrorMessage: err.message });
   }
 };
 
--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -2,18 +2,67 @@ const mongoose = require("mongoose");
 const blogModel = require("../models/blogModel");
 const authorModel = require("../models/authorModel");
 
+const isValid = function (value) {
+    if (typeof value === "undefined" || value === Number || value === null) return false
+    if (typeof value === "string" && value.trim().length === 0) return false
+    return true
+}
+
 //Create a blog document from request body. Get authorId in request body only
 exports.blogs = async function (req, res) {
-  try {
-    let blogBody = req.body;
-    let checkAuthorId = await authorModel.findById(req.body.authorId);
-    if (!checkAuthorId) {
-      return res.status(400).send({ msg: "Please Enter Valid AuthorId" });
-    } else {
-      let blogData = await blogModel.create(blogBody);
-      res.status(201).send({ data: blogData });
+    try {
+        let blogBody = req.body;
+        if (Object.keys(blogBody).length == 0) {
+            return res.status(400).send({ status: false, msg: "data is required" });
+        }
+        if (!isValid(blogBody.title)) {
+            return res.status(400).send({ status: false, msg: "title is required" });
+        }
+
+        if (!isValid(blogBody.body)) {
+            return res.status(400).send({ status: false, msg: "body is required" });
+        }
+
+        if (!isValid(blogBody.authorId)) {
+            return res.status(400).send({ status: false, msg: "authorId is required" });
+        }
+
+        if (!isValid(blogBody.tags)) {
+            return res.status(400).send({ status: false, msg: "tags is required" });
+        }
+
+        if (!isValid(blogBody.category)) {
+            return res.status(400).send({ status: false, msg: "category is required" });
+        }
+
+        if (!isValid(blogBody.subcategory)) {
+            return res.status(400).send({ status: false, msg: "subcategory is required" });
+        }
+
+        if (!isValid(blogBody.isPublished)) {
+            return res.status(400).send({ status: false, msg: "isPublished is required" });
+        }
+
+        if (!isValid(blogBody.publishedAt)) {
+            return res.status(400).send({ status: false, msg: "publishedAt is required" });
+        }
+
+        if (!isValid(blogBody.isDeleted)) {
+            return res.status(400).send({ status: false, msg: "isDeleted is required" });
+        }
+
+        if (!isValid(blogBody.deletedAt)) {
+            return res.status(400).send({ status: false, msg: "deletedAt is required" });
+        }
+
+        let checkAuthorId = await authorModel.findById(req.body.authorId);
+        if (!checkAuthorId) {
+            return res.status(400).send({ msg: "Please Enter Valid AuthorId" });
+        } else {
+            let blogData = await blogModel.create(blogBody);
+            res.status(201).send({ data: blogData });
+        }
+    } catch (err) {
+        res.status(500).send({ ErrorName: err.name, ErrorMsg: err.message });
     }
-  } catch (err) {
-    res.status(500).send({ ErrorName: err.name, ErrorMsg: err.message });
-  }
 };
