afe6d3f0a7862b70789d677d88406eccd6b7f704
KhomeshG <khomesshgajbhiye@gmail.com>
2022-09-07 17:51:04 +0530

    Did SomeChanges(By Khomesh) On Auth-Part and in PUT API

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
 const blogModel = require("../models/blogModel");
 const authorModel = require("../models/authorModel");
+const moment = require("moment");
+
+//Globals
+
+let dateToday = moment();
 
 const isValid = function (value) {
   if (typeof value === "undefined" || value === Number || value === null)
@@ -107,6 +112,11 @@ exports.blogsUpdate = async function (req, res) {
       return res.status(400).send({ msg: "ID is madatory" });
     }
 
+    //Validating Empty Document(Doc Present/Not)
+    if (Object.keys(blogBody) == 0) {
+      return res.status(400).send({ msg: "Cant Update Empty document" });
+    }
+
     //Validating BlogId(Present/Not)
 
     let checkBlogId = await blogModel.findById(req.params.blogId);
@@ -115,19 +125,33 @@ exports.blogsUpdate = async function (req, res) {
     }
 
     //Allowing Only Whose Document Is Not Delected
+
     if (checkBlogId.isDeleted == true) {
-      return res
-        .status(400)
-        .send({ msg: "if deleted is true deletedAt will have a date" });
+      return res.status(400).send({ msg: "This Document is Already Deleted" });
     }
+
     //All Validation Working
+
     //Upadting user Changes
     else {
       let blogUpdateData = await blogModel.findByIdAndUpdate(
         {
           _id: checkBlogId._id,
         },
-        blogBody,
+
+        {
+          $addToSet: { tags: blogBody.tags, subcategory: blogBody.subcategory },
+          $set: {
+            title: blogBody.title,
+            body: blogBody.body,
+            authorId: blogBody.authorId,
+            category: blogBody.category,
+            isPublished: true,
+            isDeleted: blogBody.isDeleted,
+          },
+          $currentDate: { publishedAt: dateToday.format("YYYY-MM-DD") },
+        },
+
         { new: true }
       );
       return res.status(201).send({ data: blogUpdateData });
@@ -138,27 +162,6 @@ exports.blogsUpdate = async function (req, res) {
       ErrorName: err.name,
       ErrorMessage: err.message,
     });
-  }
-};
-
-// -------------DELETE BY BOLGID ---------------
-const deleteBlogById = async function (req, res) {
-  try {
-    let blogId = req.params.blogId;
-    let blog = await blogModel.findById(blogId);
-    let data = blog.isDeleted;
-    console.log(data);
-    if (!blog) {
-      return res.status(404).send(" This is not  a valid blogId");
-    }
-
-    if (data == true) {
-      return res.status(404).send("blog document doesn't exist");
-    } else {
-      res.status(200).send({ status: 200 });
-    }
-  } catch (err) {
-    res.status(500).send({ ErrorName: err.name, ErrorMsg: err.message });
   }
 };
 
@@ -221,8 +224,7 @@ const deleteblog = async function (req, res) {
       );
 
       return res.status(200).send({ status: true, data: deleteblog });
-    }else{
-      
+    } else {
       return res.status(404).send("blog document doesn't exist");
     }
   } catch (error) {
