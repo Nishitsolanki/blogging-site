76323dc0eb4dd79a96a889caff07ab3792758f4d
KhomeshG <khomesshgajbhiye@gmail.com>
2022-09-07 17:47:13 +0530

    Did SomeChanges(By Khomesh) On Auth-Part and in PUT API

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,28 +1,33 @@
 const jwt = require("jsonwebtoken");
 const authorModel = require("../models/authorModel");
 
+exports.login = async function (req, res) {
+  try {
+    let checkEmailAndPassword = await authorModel.findOne({
+      email: req.body.email,
+      password: req.body.password,
+    });
+    //Validating Email And PassWord(present/Not)
+    if (!checkEmailAndPassword) {
+      return res.status(400).send({ msg: "Email and Password Are invalid" });
 
-const login = async function(req, res){
+      //creating Token
+    } else {
+      let payloadDetails = {
+        UserId: checkEmailAndPassword._id.toString(),
+        password: checkEmailAndPassword.password,
+        batch: "Plutonium",
+        Project: "Blogging - Site",
+        group: 30,
+      };
+      let Token = jwt.sign(payloadDetails, "FunctionUP-Project1-Group30");
+      res.header({ "x-api-key": Token });
 
-
-    let email = req.body.emailId;
-    let password = req.body.password;
-  
-    let author = await authorModel.findOne({ emailId: email, password: password });
-    if (!author)
-      return res.send({status: false, msg: "email or password is not corerct" });
-  
-    
-    let token = jwt.sign(
-      {
-        authorId: author._id.toString(),
-        batch: "plutonium",
-        organisation: "FUnctionUp",
-      },
-      "functionup-plutonium-group30"
-    );
-    res.setHeader("x-auth-token", token);
-    res.send({ status: true, data: token });
-  };
-  
-  module.exports.login = login
+      return res
+        .status(201)
+        .send({ data: Token, userId: checkEmailAndPassword._id });
+    }
+  } catch (err) {
+    res.status(500).send({ msg: "Server error HTTP 500" });
+  }
+};
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,23 +1,31 @@
 const jwt = require("jsonwebtoken");
 
-//Checking Value in (Present/Not)
+//Checking Header-Value in (Present/Not)
 exports.headerCheck = function (req, res, next) {
-  let headerData = req.headers["x-api-key"];
-  if (headerData === undefined) {
-    return res.send({ msg: "Header Is Madtory" });
-  } else {
-    next();
+  try {
+    let headerData = req.headers["x-api-key"];
+    if (headerData === undefined) {
+      return res.send({ msg: "Header Is Madtory" });
+    } else {
+      next();
+    }
+  } catch (err) {
+    res.status(500).send({ msg: "Server Error 500" });
   }
 };
 
 //Authentication Part
 exports.authentication = function (req, res, next) {
-  let Token = req.headers["x-api-key"];
-  let tokenVerify = jwt.verify(Token, "FunctionUP-Project1-Group30");
+  try {
+    let Token = req.headers["x-api-key"];
+    let tokenVerify = jwt.verify(Token, "FunctionUP-Project1-Group30");
 
-  if (tokenVerify.UserId !== req.query.authorId) {
-    return res.status(404).send({ msg: "User is Imposter" });
-  } else {
-    next();
+    if (tokenVerify.UserId !== req.query.authorId) {
+      return res.status(404).send({ msg: "User is Imposter" });
+    } else {
+      next();
+    }
+  } catch (err) {
+    res.status(500).send({ msg: "Server Error 500" });
   }
 };
