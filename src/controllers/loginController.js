c565a90a3d1afb998a592b39a9c58bd9f0207fb5
nisit <solankinisitgujarat@gmail.com>
2022-09-20 13:44:05 +0530

    final

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,5 +1,5 @@
-const jwt = require("jsonwebtoken");
 const authorModel = require("../models/authorModel");
+const jwt = require("jsonwebtoken");
 
 exports.login = async function (req, res) {
   try {
@@ -7,25 +7,29 @@ exports.login = async function (req, res) {
       email: req.body.email,
       password: req.body.password,
     });
+    console.log(checkEmailAndPassword);
     //Validating Email And PassWord(present/Not)
     if (!checkEmailAndPassword) {
-      return res.status(400).send({ status: false, msg: "Email and Password Are invalid" });
+      return res.status(400).send({ msg: "Email and Password Are invalid" });
 
       //creating Token
     } else {
       let payloadDetails = {
-        userId: checkEmailAndPassword._id.toString(),
+        UserId: checkEmailAndPassword._id.toString(),
         password: checkEmailAndPassword.password,
         batch: "Plutonium",
         Project: "Blogging - Site",
         group: 30,
       };
       let Token = jwt.sign(payloadDetails, "FunctionUP-Project1-Group30");
+
       res.header({ "x-api-key": Token });
 
-      return res.status(201).send({ status: true, data: Token, userId: checkEmailAndPassword._id });
+      return res
+        .status(201)
+        .send({ data: Token, userId: checkEmailAndPassword._id });
     }
   } catch (err) {
-    res.status(500).send({ status: false, status: false, msg: "Server error HTTP 500" });
+    res.status(500).send({ msg: "Server error HTTP 500" });
   }
 };
